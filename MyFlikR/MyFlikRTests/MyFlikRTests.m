a31beb7fbd7877a17c91423590da2430bce0906c
Mugiwaranojo <joan.g.francois@gmail.com>
2014-07-08 16:10:52 +0200

    init commit

---
+++ MyFlikR/MyFlikRTests/MyFlikRTests.m
@@ -0,0 +1,34 @@
+//
+//  MyFlikRTests.m
+//  MyFlikRTests
+//
+//  Created by fitec on 08/07/2014.
+//  Copyright (c) 2014 Mugiwaranojo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyFlikRTests : XCTestCase
+
+@end
+
+@implementation MyFlikRTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
